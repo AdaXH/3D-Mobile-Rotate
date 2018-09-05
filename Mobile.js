4ff8c75a193e38ce326bde5ad3e588405b2813e1
Ada <33694748+AdaXH@users.noreply.github.com>
2018-09-05 09:37:27 +0800

    Update Mobile.js
    参数类型有误的提示

--- Mobile.js
+++ Mobile.js
@@ -2,47 +2,49 @@
 // Web:https://adaxh.applinzi.com
 
 class Mobile {
-    constructor(option){
+    constructor(option) {
+        if (Object.prototype.toString.call(option) !== '[object Object]' && option)
+            throw Error('参数类型有误')
         const options = option || {}
-        this.items = (options && options.menu && options.menu.length !== 0) ? options.menu : [
+        this.items = (options.menu && options.menu.length !== 0) ? options.menu : [
             {
-                name : 'menu1',
-                callback : () => console.log('click menu1')
+                name: 'menu1',
+                callback: () => console.log('click menu1')
             },
             {
-                name:'menu2',
-                callback : () => console.log('click menu2')
+                name: 'menu2',
+                callback: () => console.log('click menu2')
             }
         ] //初始化菜单，以及点击菜单的操作
-        this.appendTarget = options && options.target || document.body //插件将插入的容器
-        /*手机的背景图*/     
-        this.bg = options && options.bg || 'http://www.rui2.net/uploadfile/output/3/2013/1001/4acf64758d420524.jpg'
+        this.appendTarget = options.target || document.body //插件将插入的容器
+        /*手机的背景图*/
+        this.bg = options.bg || 'http://www.rui2.net/uploadfile/output/3/2013/1001/4acf64758d420524.jpg'
         this.ul = {} //菜单列表
         this.interval = undefined //刷新时间的定时器
-        this.menuColor = options && options.menuColor || 'black' //菜单文字颜色
+        this.menuColor = options.menuColor || 'black' //菜单文字颜色
         this.searchInterval = undefined //搜索入口的特效定时器
-        this.searchCallback = options && options.searchCallback || function (search) { console.log(search) } //点击搜索触发的操作
+        this.searchCallback = options.searchCallback || function (search) { console.log(search) } //点击搜索触发的操作
         this.appendNodes = [] //需要插入手机容器的元素集合
     }
-    start(){
+    start() {
         if (document.getElementById('_container') && document.getElementById('_container').childNodes.length !== 0) //判断页面是否已经存在插件
             return
         this.init()
         setTimeout(() => { //初始化载入插件时，首次自动旋转
-            this.container.style.transform =  'rotateX(45deg) rotateY(68deg) rotateZ(-45deg) scale(1.3)'
+            this.container.style.transform = 'rotateX(45deg) rotateY(68deg) rotateZ(-45deg) scale(1.3)'
             setTimeout(() => {
                 this.container.style.transform = 'rotateX(60deg) rotateZ(45deg) rotateY(0deg) scale(1.3)'
             }, 1000);
         }, 1000);
     }
-    end(){ //清楚定时器，并移除插件
+    end() { //清楚定时器，并移除插件
         if (this.interval)
             clearInterval(this.interval)
-        if(this.searchInterval)
+        if (this.searchInterval)
             clearInterval(this.searchInterval)
         document.getElementById('_container') && this.appendTarget.removeChild(document.getElementById('_container'))
     }
-    init(){ //初始化插件
+    init() { //初始化插件
         this.createOperationUl()
         this.addMouseEnter(this.ul)
         this.appendNodes = [
@@ -57,7 +59,7 @@ class Mobile {
         this.createContainer(this.appendNodes) //创建手机容器，并将需要插入的元素的集合传递过去，在this.createContainer中插入
         this.renderTime() //刷新手机屏幕上的时间
     }
-    addMouseEnter({ ul, ul2 ,ul3}) {  //hover菜单时，侧屏hover效果
+    addMouseEnter({ ul, ul2, ul3 }) {  //hover菜单时，侧屏hover效果
         const _this = this
         const li1 = ul.getElementsByTagName('li')
         const li2 = ul2.getElementsByTagName('li')
@@ -92,91 +94,91 @@ class Mobile {
             })
         }
     }
-    createClock(){ //时间容器
+    createClock() { //时间容器
         const clock = document.createElement('div')
-        setStyle(clock , clockStyle )
+        setStyle(clock, clockStyle)
         const time = document.createElement('p')
-        time.setAttribute('id' , 'menu_plugin_clock')
-        setStyle(time , timeStyle)
+        time.setAttribute('id', 'menu_plugin_clock')
+        setStyle(time, timeStyle)
         clock.appendChild(time)
         return clock
     }
-    renderTime(){ //刷新时间
+    renderTime() { //刷新时间
         const clock = document.getElementById('menu_plugin_clock')
         const search = document.getElementById('_search')
-        this.searchInterval = setInterval(()=>{
+        this.searchInterval = setInterval(() => {
             search.style.opacity = '0.2'
-            setTimeout(()=>{
+            setTimeout(() => {
                 search.style.opacity = '1'
-            },1500)
-        },3000)
-        this.interval = setInterval(()=>{
+            }, 1500)
+        }, 3000)
+        this.interval = setInterval(() => {
             const d = new Date()
-            const h = d.getHours() < 10 ?  '0' + d.getHours() : d.getHours()
+            const h = d.getHours() < 10 ? '0' + d.getHours() : d.getHours()
             const m = d.getMinutes() < 10 ? '0' + d.getMinutes() : d.getMinutes()
             const s = d.getSeconds() < 10 ? '0' + d.getSeconds() : d.getSeconds()
             clock.innerText = '' + h + ' : ' + m + ' : ' + s
-        },1000)
+        }, 1000)
     }
-    createSearch(){ //搜索
+    createSearch() { //搜索
         const _this = this
         const search = document.createElement('div')
-        search.setAttribute('id' , '_search')
+        search.setAttribute('id', '_search')
         const input = document.createElement('input')
-        setStyle(input , inputStyle)
-        setStyle(search , searchStyle)
+        setStyle(input, inputStyle)
+        setStyle(search, searchStyle)
         input.placeholder = 'O'
-        input.addEventListener('focus', function(){
+        input.addEventListener('focus', function () {
             this.style.width = '140px'
             clearInterval(_this.searchInterval)
             this.style.paddingLeft = '20px'
             this.style.textAlign = 'left'
             this.placeholder = ''
-        },false)
-        input.addEventListener('blur', function(){
+        }, false)
+        input.addEventListener('blur', function () {
             this.style.width = '25px'
             this.style.paddingLeft = '0px'
             this.style.textAlign = 'center'
             this.placeholder = 'O'
-            _this.searchInterval = setInterval( () => {
+            _this.searchInterval = setInterval(() => {
                 search.style.opacity = '0.2'
                 setTimeout(() => {
                     search.style.opacity = '1'
                 }, 1500)
-            },3000)
-        },false)
-        input.addEventListener('keydown', function(e){
+            }, 3000)
+        }, false)
+        input.addEventListener('keydown', function (e) {
             const ev = e || window.event
-            if(ev.keyCode === 13)
-               _this.searchCallback(this.value)
-        },false)
+            if (ev.keyCode === 13)
+                _this.searchCallback(this.value)
+        }, false)
         search.appendChild(input)
         return search
     }
-    createContainer(nodes){ //创建手机的容器，以及子元素
+    createContainer(nodes) { //创建手机的容器，以及子元素
         const container = document.createElement('div')
         setStyle(container, containerStyle)
-        container.setAttribute('id' , '_container')
-        for(let item of nodes){
-            if (/Object/.test(Object.prototype.toString.call(item.call(this)))){
-                    const obj = item.call(this)
-                    for(let key in obj)
-                        container.appendChild(obj[key])
-                }else 
-                    container.appendChild(item.call(this))
+        container.setAttribute('id', '_container')
+        for (let item of nodes) {
+            if (/Object/.test(Object.prototype.toString.call(item.call(this)))) {
+                const obj = item.call(this)
+                for (let key in obj)
+                    container.appendChild(obj[key])
+            } else
+                container.appendChild(item.call(this))
         }
         this.container = container
         this.appendTarget.style.perspective = '1400px'
         this.appendTarget.style.transformStyle = 'preserve-3d'
         this.appendTarget.appendChild(container)
     }
-    createOperation(){ //手机旋转
+    createOperation() { //手机旋转
         const leftO = document.createElement('p')
         leftO.innerText = ' <'
         setStyle(leftO, leftOstyle)
-        leftO.addEventListener('click',()=>{
+        leftO.addEventListener('click', () => {
             this.container.style.transform = 'rotateX(60deg) rotateZ(45deg) rotateY(0deg) scale(1.3)'
-        },false)
+        }, false)
         const rightO = document.createElement('p')
         rightO.innerText = ' >'
         setStyle(rightO, rightOstyle)
@@ -191,7 +193,7 @@ class Mobile {
         setStyle(closeO, closeOStyle)
         return { leftO, rightO, closeO }
     }
-    createTopface(){ //手机的上面
+    createTopface() { //手机的上面
         const top = document.createElement('div')
         const barrery = document.createElement('div')
         const barreryStatus = document.createElement('div')
@@ -231,7 +233,7 @@ class Mobile {
         left.appendChild(this.ul.ul3)
         return left
     }
-    createRight(){ //手机的右面
+    createRight() { //手机的右面
         const right = document.createElement('div')
         setStyle(right, rightStyle)
         right.style.background = `url(${this.bg}) right`
@@ -253,18 +255,18 @@ class Mobile {
         forward.style.backgroundSize = 'cover'
         return forward
     }
-    createOperationUl(){ //菜单
+    createOperationUl() { //菜单
         const ul = document.createElement('ul')
         const ul2 = document.createElement('ul')
         const ul3 = document.createElement('ul')
-        for(let item of this.items){
+        for (let item of this.items) {
             const li = document.createElement('li')
             const li2 = document.createElement('li')
             const li3 = document.createElement('li')
             li.innerText = item.name
-            if(item.callback)
-                li.addEventListener('click', item.callback , false)
-            ul.appendChild(setStyle(li , li1Style))
+            if (item.callback)
+                li.addEventListener('click', item.callback, false)
+            ul.appendChild(setStyle(li, li1Style))
             li.style.color = this.menuColor
             li2.style.height = '30px'
             li2.style.transition = 'all ease .3s'
@@ -278,33 +280,33 @@ class Mobile {
         setStyle(ul, ul1Style)
         setStyle(ul2, ul2Style)
         setStyle(ul3, ul2Style)
-        this.ul =  { ul, ul2, ul3 }
+        this.ul = { ul, ul2, ul3 }
     }
 }
 
 // function
 
-function recover(els, keys){ //恢复el的初识样式
-    for(let item of els)
-        for(let key in keys)
+function recover(els, keys) { //恢复el的初识样式
+    for (let item of els)
+        for (let key in keys)
             item.style[key] = keys[key]
 }
 
-function setStyle(el , styles) { //设置el样式
-    if(!el || !styles ) return null
-    if(!styles) return el
-    for(let key in styles)
+function setStyle(el, styles) { //设置el样式
+    if (!el || !styles) return null
+    if (!styles) return el
+    for (let key in styles)
         el.style[key] = styles[key]
     return el
 }
 /* 对应的元素的样式  */
 
 const leftOstyle = {
-    position : 'absolute',
-    left : '300px',
-    bottom : '50px',
-    cursor : 'pointer',
-    fontSize : '20px',
+    position: 'absolute',
+    left: '300px',
+    bottom: '50px',
+    cursor: 'pointer',
+    fontSize: '20px',
     'text-shadow': '0px 10px 10px rgba(6, 5, 5, 0.9)',
 }
 
@@ -347,64 +349,64 @@ const barreryStatusStyle = {
 }
 
 const closeOStyle = {
-    position : 'absolute',
+    position: 'absolute',
     bottom: '50px',
     cursor: 'pointer',
     fontSize: '20px',
     'text-shadow': '0px 10px 10px rgba(6, 5, 5, 0.9)',
-    left : '400px'    
+    left: '400px'
 }
 
 const rightOstyle = {
     position: 'absolute',
     right: '200px',
     bottom: '50px',
-    cursor : 'pointer',
-    fontSize : '20px',
+    cursor: 'pointer',
+    fontSize: '20px',
     'text-shadow': '0px 10px 10px rgba(6, 5, 5, 0.9)',
 }
 
 const clockStyle = {
-    position : ' absolute',
+    position: ' absolute',
     width: '200px',
-    height : '100px',
-    top : '20px'
+    height: '100px',
+    top: '20px'
 }
 
 const inputStyle = {
-    outline : 'none',
-    border : 'none',
-    display : 'block',
-    margin : '0px auto',
-    cursor : 'pointer',
-    textAlign : 'center',
-    width : '25px',
-    transition : 'all .3s',
-    height : '25px',
-    background : 'rgba(255,255,255,0.7)',
-    borderRadius : '12.5px',
-    color : '#666',
-    fontSize : '12px'
+    outline: 'none',
+    border: 'none',
+    display: 'block',
+    margin: '0px auto',
+    cursor: 'pointer',
+    textAlign: 'center',
+    width: '25px',
+    transition: 'all .3s',
+    height: '25px',
+    background: 'rgba(255,255,255,0.7)',
+    borderRadius: '12.5px',
+    color: '#666',
+    fontSize: '12px'
 }
 
 const searchStyle = {
-    willChange : 'opacity',
-    width : '200px',
-    height : '30px',
-    position : 'absolute',
-    top : '130px',
-    opacity : '0.2',
-    filter : 'alpha(opacity = 20)',
-    transition : 'all 1s',
+    willChange: 'opacity',
+    width: '200px',
+    height: '30px',
+    position: 'absolute',
+    top: '130px',
+    opacity: '0.2',
+    filter: 'alpha(opacity = 20)',
+    transition: 'all 1s',
     'transform-style': 'preserve-3d',
     'transform': 'translateZ(1px)'
 }
 
 const timeStyle = {
-    textAlign : 'center',
-    lineHeight : '100px',
-    fontSize : '20px',
-    color : 'white'
+    textAlign: 'center',
+    lineHeight: '100px',
+    fontSize: '20px',
+    color: 'white'
 }
 
 const ul2Style = {
@@ -417,25 +419,25 @@ const ul2Style = {
 const ul1Style = {
     position: 'absolute',
     bottom: '20px',
-    left : '0',
+    left: '0',
     cursor: 'pointer',
-    minHeight : '70px',
+    minHeight: '70px',
     'transform-style': 'preserve-3d',
     'transform': 'translateZ(0.1px)'
-} 
+}
 
 const li1Style = {
-    transformStyle : 'preserve-3d',
-    transform : 'translateX(-1px)',
+    transformStyle: 'preserve-3d',
+    transform: 'translateX(-1px)',
     padding: '0 0 0 10px',
     height: '30px',
     'line-height': '30px',
     width: '200px',
     // 'border-bottom': '1px solid #eee',
-    fontSize : '12px',
-    transition : 'all ease .3s',
-    'box-sizing':'border-box',
-    cursor : 'pointer'
+    fontSize: '12px',
+    transition: 'all ease .3s',
+    'box-sizing': 'border-box',
+    cursor: 'pointer'
 }
 
 const bottomStyle = {
@@ -449,7 +451,7 @@ const bottomStyle = {
     height: '20px',
     'transform-origin': 'left',
     'background-size': 'auto',
-    borderRadius : '10px',
+    borderRadius: '10px',
 }
 
 const forwardStyle = {
@@ -463,7 +465,7 @@ const forwardStyle = {
     height: '20px',
     'transform-origin': 'left',
     'background-size': 'auto',
-    borderRadius: '10px',   
+    borderRadius: '10px',
 }
 
 const rightStyle = {
@@ -476,7 +478,7 @@ const rightStyle = {
     top: '50px',
     'background-size': 'cover',
     transform: 'rotateY(90deg) translateZ(10px)  translateX(10px)',
-    borderRadius : '10px',
+    borderRadius: '10px',
 }
 
 const leftStyle = {
@@ -503,10 +505,10 @@ const topStyle = {
     'background-size': 'cover',
     border: '0.5px solid white',
     position: 'absolute',
-    borderRadius : '5px',
+    borderRadius: '5px',
     'box-sizing': 'border-box',
-    transformStyle : 'preserve-3d',
-    transform : 'translateZ(0px)'
+    transformStyle: 'preserve-3d',
+    transform: 'translateZ(0px)'
 }
 
 const backStyle = {
@@ -521,7 +523,7 @@ const backStyle = {
     'box-sizing': 'border-box',
     'box-shadow': '0px 50px 50px rgba(6, 5, 5, 0.9)',
     transformStyle: 'preserve-3d',
-    transform: 'translateZ(-20px)'    
+    transform: 'translateZ(-20px)'
 }
 
 const shadowStyle = {
@@ -533,8 +535,8 @@ const shadowStyle = {
     'transform-origin': 'top',
     position: 'absolute',
     'box-sizing': 'border-box',
-    background : 'transparent',
-    transform : 'translateZ(-100px)'
+    background: 'transparent',
+    transform: 'translateZ(-100px)'
 }
 
 const containerStyle = {
@@ -543,11 +545,11 @@ const containerStyle = {
     height: '600px',
     position: 'absolute',
     top: '50%',
-    zIndex : '5',
+    zIndex: '5',
     left: '41%',
     margin: '-300px 0 0 -350px',
     perspective: '1400px',
     'transform-style': 'preserve-3d',
     'transform-origin': 'center',
-    transition : 'all 1s'
+    transition: 'all 1s'
 }
